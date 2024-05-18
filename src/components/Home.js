6abd904bd5b949e022c6ef3e938d1f462455e515
ArturGrilo <arturgriloo1994@hotmail.com>
2024-05-18 21:11:49 +0200

    Responsivness  v7.95

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "../styles/Home.css";
 
 const Home = () => {
   const [imageIndex, setImageIndex] = useState(0);
-  const images = ["/images/HomeOutside5.jpeg", "/images/HomeInside2.jpeg", "/images/HomePageImage.jpeg"];
+
+  const images = useMemo(() => [
+    "/images/HomeOutside5.jpeg",
+    "/images/HomeInside2.jpeg",
+    "/images/HomePageImage.jpeg"
+  ], []); // Empty dependency array ensures that images is only initialized once
 
   useEffect(() => {
     const interval = setInterval(() => {
       setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Mudar a imagem a cada 5 segundos
-  
+    }, 5000); // Change the image every 5 seconds
+
     return () => clearInterval(interval);
-  }, [images]); // Remova ".length" aqui
+  }, [images]); // Depend on images to rerun the effect when it changes
 
   return (
     <section id="home-id" className="home-section">
       {images.map((img, index) => (
         <div key={index} className={`homepage-image ${index === imageIndex ? "fade-in" : "fade-out"}`}>
-          <img src={img} alt={`Imagem ${index + 1}`} className="homepage-image-animation"/> {/* Mude para "img" aqui */}
+          <img src={img} alt={`Brief description of the content`} className="homepage-image-animation"/>
         </div>
       ))}
       <div className="home-main-container"></div>
