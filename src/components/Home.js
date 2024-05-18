ffc97dcf946abe8ce2983e208c21f04599305bc6
ArturGrilo <arturgriloo1994@hotmail.com>
2024-05-18 20:58:34 +0200

    Responsivness  v7.94

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import "../styles/Home.css";
 
 const Home = () => {
   const [imageIndex, setImageIndex] = useState(0);
-  const images = ["/images/HomeOutside_2.jpeg", "/images/HomeInside2.jpeg", "/images/RC_15.jpeg", "/images/CasaDaVilaI/IMG-20240423-WA0021.jpg"];
+  const images = ["/images/HomeOutside5.jpeg", "/images/HomeInside2.jpeg", "/images/HomePageImage.jpeg"];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -11,13 +11,13 @@ const Home = () => {
     }, 5000); // Mudar a imagem a cada 5 segundos
   
     return () => clearInterval(interval);
-  }, [images.length]); 
+  }, [images]); // Remova ".length" aqui
 
   return (
     <section id="home-id" className="home-section">
       {images.map((img, index) => (
         <div key={index} className={`homepage-image ${index === imageIndex ? "fade-in" : "fade-out"}`}>
-          <img src={images[imageIndex]} alt={`Imagem ${imageIndex + 1}`} className="homepage-image-animation"/>
+          <img src={img} alt={`Imagem ${index + 1}`} className="homepage-image-animation"/> {/* Mude para "img" aqui */}
         </div>
       ))}
       <div className="home-main-container"></div>
