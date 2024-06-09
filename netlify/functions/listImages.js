94c42561db5a63e034e26c908a442e60ce880e35
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-09 23:50:39 +0200

    Prod images issue v1.15

--- netlify/functions/listImages.js
+++ netlify/functions/listImages.js
@@ -4,26 +4,23 @@ const path = require('path');
 exports.handler = async function(event, context) {
   const category = event.queryStringParameters.category || 'CasaDaVilaI';
 
-  const baseDir = path.join(__dirname, '..', '..', 'build', 'images');
-  const imagesDir = path.join(baseDir, category);
+  const imagesDir = path.join(__dirname, '..', '..', 'build', 'images', category);
 
   console.log(`Category: ${category}`);
   console.log(`Images directory: ${imagesDir}`);
+  // Caminho da raiz do projeto
+  const rootDir = path.join(__dirname, '..', '..');
+
+  console.log(`Root directory: ${rootDir}`);
+  const folders = fs.readdirSync(rootDir).filter(file => fs.statSync(path.join(rootDir, file)).isDirectory());
+  console.log(`Folders in root directory: ${folders}`);
 
   try {
-    // Listar todas as pastas no diretório de imagens
-    const folders = fs.readdirSync(baseDir).filter(file => fs.statSync(path.join(baseDir, file)).isDirectory());
-    console.log(`Available folders: ${folders}`);
-
-    // Listar os arquivos na pasta da categoria específica
     const files = fs.readdirSync(imagesDir);
     console.log(`Files: ${files}`);
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        availableFolders: folders,
-        files: files,
-      }),
+      body: JSON.stringify(files),
     };
   } catch (error) {
     console.error('Error reading images directory:', error);
