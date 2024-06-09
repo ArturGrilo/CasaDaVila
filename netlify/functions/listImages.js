ddf65c7c73b8e37aff563d2f6522e9338e701247
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-09 23:39:04 +0200

    Prod images issue v1.14

--- netlify/functions/listImages.js
+++ netlify/functions/listImages.js
@@ -4,17 +4,26 @@ const path = require('path');
 exports.handler = async function(event, context) {
   const category = event.queryStringParameters.category || 'CasaDaVilaI';
 
-  const imagesDir = path.join(__dirname, '..', '..', 'build', 'images', category);
+  const baseDir = path.join(__dirname, '..', '..', 'build', 'images');
+  const imagesDir = path.join(baseDir, category);
 
   console.log(`Category: ${category}`);
   console.log(`Images directory: ${imagesDir}`);
 
   try {
+    // Listar todas as pastas no diretório de imagens
+    const folders = fs.readdirSync(baseDir).filter(file => fs.statSync(path.join(baseDir, file)).isDirectory());
+    console.log(`Available folders: ${folders}`);
+
+    // Listar os arquivos na pasta da categoria específica
     const files = fs.readdirSync(imagesDir);
     console.log(`Files: ${files}`);
     return {
       statusCode: 200,
-      body: JSON.stringify(files),
+      body: JSON.stringify({
+        availableFolders: folders,
+        files: files,
+      }),
     };
   } catch (error) {
     console.error('Error reading images directory:', error);
