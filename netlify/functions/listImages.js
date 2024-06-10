c15ce6d4b7ae62fc3ee91d0127fd1f7f6a8d5ba6
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-10 12:04:52 +0200

    Prod images issue v1.17

--- netlify/functions/listImages.js
+++ netlify/functions/listImages.js
@@ -4,18 +4,21 @@ const path = require('path');
 exports.handler = async function(event, context) {
   const category = event.queryStringParameters.category || 'CasaDaVilaI';
 
-  const imagesDir = path.join(__dirname, '..', '..', 'build', 'images', category);
-
-  console.log(`Category: ${category}`);
-  console.log(`Images directory: ${imagesDir}`);
   // Caminho da raiz do projeto
   const rootDir = path.join(__dirname, '..', '..');
 
+  console.log(`Category: ${category}`);
   console.log(`Root directory: ${rootDir}`);
-  const folders = fs.readdirSync(rootDir).filter(file => fs.statSync(path.join(rootDir, file)).isDirectory());
-  console.log(`Folders in root directory: ${folders}`);
 
   try {
+    // Listar as pastas na raiz do projeto
+    const folders = fs.readdirSync(rootDir).filter(file => fs.statSync(path.join(rootDir, file)).isDirectory());
+    console.log(`Folders in root directory: ${folders}`);
+
+    // Caminho da pasta de imagens
+    const imagesDir = path.join(rootDir, 'build', 'images', category);
+    console.log(`Images directory: ${imagesDir}`);
+
     const files = fs.readdirSync(imagesDir);
     console.log(`Files: ${files}`);
     return {
@@ -23,10 +26,10 @@ exports.handler = async function(event, context) {
       body: JSON.stringify(files),
     };
   } catch (error) {
-    console.error('Error reading images directory:', error);
+    console.error('Error reading directory:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to read images directory', details: error.message }),
+      body: JSON.stringify({ error: 'Failed to read directory', details: error.message }),
     };
   }
 };
