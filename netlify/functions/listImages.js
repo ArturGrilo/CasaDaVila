b63d09078d32784fe37df255b37326486784256d
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-10 13:07:28 +0200

    Prod images issue v1.20

--- netlify/functions/listImages.js
+++ netlify/functions/listImages.js
@@ -1,52 +1,12 @@
-exports.handler = async (event, context) => {
-  const fs = require('fs');
-  const path = require('path');
-
-  try {
-    // Utilize o __dirname para construir o caminho absoluto corretamente
-    const directoryPath = path.join('public', 'images', 'CasadaVilaI');
-    
-    const files = fs.readdirSync(directoryPath);
-    
-    return {
-      statusCode: 200,
-      body: JSON.stringify(files),
-    };
-  } catch (error) {
-    console.error('Error reading directory:', error.message);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        errorType: error.name,
-        errorMessage: error.message,
-      }),
-    };
-  }
-};
-
-
-/*const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
   const category = event.queryStringParameters.category || 'CasaDaVilaI';
 
-  // Caminho da raiz do projeto
-  const rootDir = path.join('__dirname');
-
-  console.log(`Category: ${category}`);
-  console.log(`Root directory: ${rootDir}`);
-  // Listar as pastas na raiz do projeto
-  const folders = fs.readdirSync(rootDir).filter(file => fs.statSync(path.join(rootDir, file)).isDirectory());
-  console.log(`Folders in root directory: ${folders}`);
-
   try {
-    // Caminho da pasta de imagens
-    const imagesDir = path.join(rootDir, 'images', category);
-    console.log(`Images directory: ${imagesDir}`);
-
+    const imagesDir = path.join('build', 'images', category);
     const files = fs.readdirSync(imagesDir);
-    console.log(`Files: ${files}`);
     return {
       statusCode: 200,
       body: JSON.stringify(files),
@@ -58,4 +18,4 @@ exports.handler = async function(event, context) {
       body: JSON.stringify({ error: 'Failed to read directory', details: error.message }),
     };
   }
-}; */
+};
