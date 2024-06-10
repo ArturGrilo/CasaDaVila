d7446752e97fdd3847dd1293df09b09dc1f42e0a
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-10 12:40:01 +0200

    Prod images issue v1.19

--- netlify/functions/listImages.js
+++ netlify/functions/listImages.js
@@ -1,11 +1,38 @@
-const fs = require('fs');
+exports.handler = async (event, context) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  try {
+    // Utilize o __dirname para construir o caminho absoluto corretamente
+    const directoryPath = path.join('public', 'images', 'CasadaVilaI');
+    
+    const files = fs.readdirSync(directoryPath);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(files),
+    };
+  } catch (error) {
+    console.error('Error reading directory:', error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        errorType: error.name,
+        errorMessage: error.message,
+      }),
+    };
+  }
+};
+
+
+/*const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
   const category = event.queryStringParameters.category || 'CasaDaVilaI';
 
   // Caminho da raiz do projeto
-  const rootDir = path.join(__dirname, '..', '..');
+  const rootDir = path.join('__dirname');
 
   console.log(`Category: ${category}`);
   console.log(`Root directory: ${rootDir}`);
@@ -15,7 +42,7 @@ exports.handler = async function(event, context) {
 
   try {
     // Caminho da pasta de imagens
-    const imagesDir = path.join(rootDir, 'build', 'images', category);
+    const imagesDir = path.join(rootDir, 'images', category);
     console.log(`Images directory: ${imagesDir}`);
 
     const files = fs.readdirSync(imagesDir);
@@ -31,4 +58,4 @@ exports.handler = async function(event, context) {
       body: JSON.stringify({ error: 'Failed to read directory', details: error.message }),
     };
   }
-};
+}; */
