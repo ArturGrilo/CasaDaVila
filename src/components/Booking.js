68fecd13c28b65f597a2bc1fb4b0d501c39467ad
ArturGrilo <arturgriloo1994@hotmail.com>
2024-06-15 00:29:05 +0200

    Prod images issue v1.27

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,20 +1,43 @@
 import '../styles/Bookings.css';
 import TopBar from "../components/TopBar";
 import Footer from "../components/Footer";
+import React, { useEffect, useCallback } from "react";
 
 const BookingPage = () => {
 
+  const applyBackgroundImage = useCallback(() => {
+      const elements = document.querySelectorAll('.cdv-img-parallax-alt-page');
+      if (elements.length > 0) {
+          elements.forEach(element => {
+          element.style.backgroundImage = `url(/images/reservar.jpeg)`;
+          });
+          return true;
+      }
+      return false;
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const interval = setInterval(() => {
+    if (applyBackgroundImage()) {
+        clearInterval(interval); // Stop checking once the image is applied
+    }
+    }, 100); // Check every 100ms
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [applyBackgroundImage]);
+
   return (
     <section id="booking-page-id" className="cdv-section">
-      <div className="cdv-img-gallery-page">
-        <div className="cdv-title">
-          <span>Reservar</span>
-        </div>
-        <div className="cdv-img-parallax-gallery-page">
-          <div className='cdv-red'></div>
-        </div>
+      <div className="cdv-img-alt-page">
+          <div className="cdv-title">
+              <span>Reservar</span>
+          </div>
+          <div className="cdv-img-parallax-alt-page">
+              <div className='cdv-red'></div>
+          </div>
       </div>
-      <TopBar scrollThreshold={-1} altScreen={true} />
+      <TopBar altScreen={true} />
       <div className="cdv-main-container">
         
       </div>
