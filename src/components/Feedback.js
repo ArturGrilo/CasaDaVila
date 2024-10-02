aea5f1e968c058ba0f4840db4f5b2cff3cd87960
ArturGrilo <arturgriloo1994@hotmail.com>
2024-10-02 19:57:01 +0200

    PageSpeed issues v1.28

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -53,7 +53,7 @@ const Feedback = () => {
           ))}
         </div>
         <div className="feedback-image">
-          <img loading="lazy" src={"/images/Feedback/FeedbackImage.webp"} alt="feedback - Casa da Vila - Alojamento Local , Alpedrinha , Beira Baixa , Sintra da Beira" />
+          <img src={"/images/Feedback/FeedbackImage.webp"} alt="feedback - Casa da Vila - Alojamento Local , Alpedrinha , Beira Baixa , Sintra da Beira" />
         </div>
       </div>
     </section>
