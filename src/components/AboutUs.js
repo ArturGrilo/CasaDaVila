afd26d3dcbbe2258f52eb80c885466185006b766
ArturGrilo <arturgriloo1994@hotmail.com>
2025-05-04 18:59:24 +0200

    2025 v3.0

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,16 +8,30 @@ function AboutUs() {
   return (
     <section id="about-us-id" className="cdv-section">
       <div className="cdv-main-container">
-        <div className="cdv-title" data-aos="fade-up">
-          <span>{t('about-us.title')}</span>
+        <div className='cdv-about-us-container'>
+          <img className='cdv-about-us-image' src="/images/SobreNos/D_Sao_CasaDaVila_Alpedrinha.png" alt="Alpedrinha - Casa da Vila - Alojamento Local , Alpedrinha , Beira Baixa , Sintra da Beira" data-aos="fade-up"/>
+          
+          <div className="cdv-title-and-text">
+            <div className="cdv-title" data-aos="fade-up">
+              <span className='cdv-small-title'>Casa da Vila</span>
+              <span>{t('about-us.title')}</span>
+            </div>
+            <div className='cdv-text'>
+              <span data-aos="fade-up">{t('about-us.paragraph1')}</span>
+              <span data-aos="fade-up">{t('about-us.paragraph2')}</span>
+              <span className="not-phone" data-aos="fade-up">{t('about-us.paragraph3')}</span>
+              <span className="not-phone" data-aos="fade-up">{t('about-us.paragraph4')}</span>
+            </div>
+            <div className="about-us-signature not-phone" data-aos="fade-up">{t('about-us.signature')}</div>
+          </div>
         </div>
-        <div className="cdv-text">
-          <span data-aos="fade-up">{t('about-us.paragraph1')}</span>
-          <span data-aos="fade-up">{t('about-us.paragraph2')}</span>
-          <span data-aos="fade-up">{t('about-us.paragraph3')}</span>
-          <span data-aos="fade-up">{t('about-us.paragraph4')}</span>
+        <div className='cdv-about-us-container not-desktop'>
+          <div className='cdv-text'>
+            <span data-aos="fade-up">{t('about-us.paragraph3')}</span>
+            <span data-aos="fade-up">{t('about-us.paragraph4')}</span>
+            <div className="about-us-signature not-desktop" data-aos="fade-up">{t('about-us.signature')}</div>
+          </div>
         </div>
-        <div className="about-us-signature" data-aos="fade-up">{t('about-us.signature')}</div>
       </div>
     </section>
   );
